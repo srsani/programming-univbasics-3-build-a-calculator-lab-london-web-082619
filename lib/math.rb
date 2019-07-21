ad9a3ae676aa7f16c0a57e8dc8d4932b8473a4fa
droll-accelerator-1483 <srsani@gmail.com>
2019-07-21 08:09:15 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,31 @@
 def addition(num1, num2)
-
+  sum = num1+num2
+  return sum
 end
 
 def subtraction(num1, num2)
+    sum = num1-num2
+  return sum
 
 end
 
 def division(num1, num2)
+  sum = num1/num2
+  return sum
 
 end
 
 def multiplication(num1, num2)
-
+  sum = num1*num2
+  return sum
 end
 
 def modulo(num1, num2)
-
+  sum = num1%num2
+  return sum
 end
 
 def square_root(num)
-
+  sum = num**0.5
+  return sum
 end
